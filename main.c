3882e2ba368cb3dfa8cd06b06f57d7efa79fe881
Adam <adam.deruelle@gmail.com>
2023-11-11 13:05:51 +0100

    Définition des prototypes et de la boucle principale

--- main.c
+++ main.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
-    int tableau[[]];
-}tGrille;
+int possible(int grille[9][9], int ligne, int colonne, int valeur);
+void chargerGrille(int grille[9][9]);
+void afficherGrille(int grille[9][9]);
+void saisir(int *valeur);
 
-
-int main(){
-    tGrille grille1[81];
+int main() {
+    int grille1[9][9];
     int numLigne, numColonne, valeur;
+
     chargerGrille(grille1);
-    while (condition) {
+
+    while (!grillePleine(grille1)) {
         afficherGrille(grille1);
-        ecrireEcran("Indices de la case ? ");
-        saisir(numLigne);
-        saisir(numColonne);
-        if (grille1[numLigne][numColonne]!=0){
-            ecrireEcran("IMPOSSIBLE, la case n'est pas libre.");
-        }
-        else {
-            ecrireEcran("Valeur à insérer ? ");
-            saisir(valeur);
-            if (possible(grille1, numLigne, numColonne, valeur)){
+
+        // Saisie des indices de la case
+        printf("Indices de la case ? ");
+        saisir(&numLigne);
+        saisir(&numColonne);
+
+        // Vérification si la case est libre
+        if (grille1[numLigne][numColonne] != 0) {
+            printf("IMPOSSIBLE, la case n'est pas libre.\n");
+        } else {
+            // Saisie de la valeur à insérer
+            saisir(&valeur);
+
+            // Vérification de la validité de la valeur
+            if (possible(grille1, numLigne, numColonne, valeur)) {
                 grille1[numLigne][numColonne] = valeur;
             }
         }
     }
-    ecrireEcran("Grille pleine, fin de partie");
+
+    printf("Grille pleine, fin de partie\n");
+
     return EXIT_SUCCESS;
 }
